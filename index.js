677ab8bad95e3ef97149a51d2d7248732c422bd5
Catevika <dominique.bello@outlook.fr>
2022-12-14 17:55:28 +0100

    Add path

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 import authRoute from './routes/authRoute.js';
 
@@ -11,6 +12,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
+mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGO_URI, () =>
 	console.log('\x1b[33m', 'MongoDB connected successfully')
 );
@@ -25,3 +27,11 @@ app.use('/', authRoute);
 app.listen(process.env.EXPRESS_PORT, () =>
 	console.log('\x1b[33m', `Server running on port ${process.env.EXPRESS_PORT}`)
 );
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/dist'));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+	);
+}
