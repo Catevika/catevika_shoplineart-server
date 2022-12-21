555585c96c6653c01b1f7bd2552ab1592e4b3aa9
Catevika <dominique.bello@outlook.fr>
2022-12-21 14:21:23 +0100

    Fix 404 on refresh

--- index.js
+++ index.js
@@ -28,7 +28,9 @@ app.listen(process.env.EXPRESS_PORT, () =>
 );
 
 if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('dist'));
+
 	app.get('*', (req, res) =>
-		res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'))
+		res.sendFile(path.resolve(process.cwd(), 'dist', 'index.html'))
 	);
 }
